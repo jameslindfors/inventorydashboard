58850c9fce46fb6a41067e376d373a30a4486fe2
jameslindfors <jameslindfors@outlook.com>
2022-09-13 23:27:47 -0700

    ORM Model and Route

--- api/inventory-service/models/product_model.py
+++ api/inventory-service/models/product_model.py
@@ -1,4 +1,3 @@
-from time import timezone
 from tortoise import fields
 from tortoise.models import Model
 
@@ -7,7 +6,21 @@ from tortoise.contrib.pydantic import pydantic_model_creator
 # Tortoise ORM Model
 class Product(Model):
     id = fields.IntField(pk=True)
+    
     name = fields.CharField(50, unique=True)
+    description = fields.CharField(50)
+    image = fields.CharField(50)
+    
+    price = fields.FloatField()
+    tax_rate = fields.FloatField()
+    shipping_rate = fields.FloatField()
+    currency = fields.CharField(50)
+    
+    total_units = fields.IntField()
+    current_units = fields.IntField()
+    
+    sale = fields.BooleanField()
+    sale_percent = fields.FloatField()
 
 # Pydantic Model
 Product_Pydantic = pydantic_model_creator(Product, name='Product')
--- api/inventory-service/routers/product_route.py
+++ api/inventory-service/routers/product_route.py
@@ -1,34 +1,53 @@
+from http.client import HTTPException
+from itertools import product
 from fastapi import APIRouter
+from tortoise.contrib.fastapi import HTTPNotFoundError
+from models.product_model import Product, ProductIn_Pydantic, Product_Pydantic
 
 router = APIRouter()
 
-@router.get('/get/{product_id}', tags=['product'])
-def get_product_by_id():
-    return 'Get product by id'
+@router.get(
+    '/get/{product_id}', response_model=Product_Pydantic, responses={404: {"model": HTTPNotFoundError}}, tags=['product']
+)
+async def get_product_by_id(product_id: int):
+    return await Product_Pydantic.from_queryset_single(Product.get(id=product_id))
 
-@router.get('/get/collection/{collection_id}', tags=['product', 'collection'])
-def get_collection_by_id():
+@router.get(
+    '/get/collection/{collection_id}', response_model=Product_Pydantic, responses={404: {'model': HTTPNotFoundError}}, tags=['product', 'collection']
+)
+async def get_collection_by_id(collection_id: int):
     return "Get all by collection"
 
-@router.post('/create', status_code=201, tags=['product'])
-def create_products():
-    return "Create Products"
+@router.post(
+    '/create', response_model=Product_Pydantic, status_code=201, tags=['product']
+)
+async def create_products(product: ProductIn_Pydantic):
+    product_obj = await Product.create(**product.dict(exclude_unset=True))
+    return await Product_Pydantic.from_tortoise_orm(product_obj)
 
 @router.post('/create/collection', status_code=201, tags=['product', 'collection'])
 def create_collection():
     return "Create collection"
 
-@router.put('/update/{product_id}', tags=['product'])
-def update_product_by_id():
-    return "Update product by id"
+@router.put(
+    '/update/{product_id}', response_model=Product_Pydantic, tags=['product']
+)
+async def update_product_by_id(product_id: int, product: ProductIn_Pydantic):
+    await Product.filter(id=product_id).update(**product.dict(exclude_unset=True))
+    return await Product_Pydantic.from_queryset_single(Product.get(id=product_id))
 
 @router.put('/update/collection/{collection_id}', tags=['product', 'collection'])
 def update_collection_by_id():
     return "Update collection by id"
 
-@router.delete('/delete/{product_id}', tags=['product'])
-def delete_product_by_id():
-    return "Delete product by id"
+@router.delete(
+    '/delete/{product_id}', responses={404: {'model': HTTPNotFoundError}}, tags=['product']
+)
+async def delete_product_by_id(product_id: int):
+    deleted = await Product.filter(id=product_id).delete()
+    if not deleted: 
+        raise HTTPException(status_code=404, detail=f"Product {product_id} not found")
+    return {"status": f"{product_id} deleted successfully"}
 
 @router.delete('/delete/collection/{collection_id}', tags=['product', 'collection'])
 def delete_collection_by_id():
