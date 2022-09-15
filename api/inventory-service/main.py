7509fc9a26936430b13e2ef5a21fd6c4d8d35752
jameslindfors <jameslindfors@outlook.com>
2022-09-14 20:29:39 -0700

    Register Routes

--- api/inventory-service/main.py
+++ api/inventory-service/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import product_route, inventory_route
+from routers import product_route, inventory_route, collection_route, util_route
 from db import database
 from config import settings
 import os
@@ -20,9 +20,16 @@ async def startup():
     database.init_db(app)
 
 product_enabled = os.getenv('PRODUCT_ENABLED', 'y')
+collection_enabled = os.getenv('COLLECTION_ENABLED', 'y')
 inventory_enabled = os.getenv('INVENTORY_ENABLED', 'y') 
+utils_enabled = os.getenv('UTILS_ENABLED', 'y')
+
 
 if product_enabled == 'y':
     app.include_router(product_route.router, prefix=f'/{settings.URI_PREFIX}/p')
+if collection_enabled == 'y': 
+    app.include_router(collection_route.router, prefix=f'/{settings.URI_PREFIX}/c')
 if inventory_enabled == 'y':
     app.include_router(inventory_route.router, prefix=f'/{settings.URI_PREFIX}/i')
+if utils_enabled == 'y':
+    app.include_router(util_route.router, prefix=f'/{settings.URI_PREFIX}/u')
